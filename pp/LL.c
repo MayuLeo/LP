4929b090c3e68a39d1ac7b1d24c9abc47dbf7b89
Leo Mayuzumi <leoleomayuzumi@gmail.com>
2018-11-08 01:01:20 +0900

    複合文の実装

---
+++ pp/LL.c
@@ -0,0 +1,94 @@
+#include "token-list.h"
+extern int token;
+int parse_program() {
+  printf("Token %d\n",token);
+  if(token != TPROGRAM) return(error("Keyword 'program' is not found"));
+  token = scan();
+  if(token != TNAME) return(error("Program name is not found"));
+  token = scan();
+  if(token != TSEMI) return(error("Semicolon is not found"));
+  token = scan();
+  if(block() == ERROR) return(ERROR);
+  if(token != TDOT) return(error("Period is not found at the end of program"));
+  token = scan();
+  return(NORMAL);
+}
+int block()//ブロック{ 変数宣言部 | 副プログラム宣言 } 複合文
+{
+  printf("block\n");
+  while(token == TPROCEDURE || token == TVAR)
+  {
+    if(token == TPROCEDURE)
+    {
+      if(subprogram_declaration() == ERROR) return(ERROR);
+    }
+    if(token == TVAR)
+    {
+      if(variable_declaration() == ERROR) return(ERROR);
+    }
+
+  }
+  if(compound_statement() == ERROR) return(ERROR);
+  return(NORMAL);
+}
+int variable_declaration()//変数宣言文:"var" 変数名の並び ":" 型 ";" { 変数名の並び ":" 型 ";" }
+{
+  printf("variable_declaration\n");
+
+  return(NORMAL);
+}
+int subprogram_declaration()//副プログラム宣言:"procedure" 手続き名 [ 仮引数部 ] ";" [ 変数宣言部 ] 複合文 ";"
+{
+  printf("subprogram_declaration\n");
+  if (token != TPROCEDURE) return(error("procedure is not found."));
+  token = scan();
+  //if (token != TNAME) return(error("procedure name is keyward."));
+  if(procedure_name() == ERROR) return(ERROR);
+  token = scan();
+  if(token == TLPAREN)
+  {
+    if(formal_parameters() == ERROR) return(ERROR);
+    token = scan();
+  }
+  if(token != TSEMI) return(error("semicolon is not found"));
+  token = scan();
+  if(token == TVAR)
+  {
+    if(variable_declaration() == ERROR) return(ERROR);
+    token = scan();
+  }
+
+  if(compound_statement() == ERROR) return(ERROR);
+  token = scan();
+  if(token != TSEMI) return(error("semicolon is not found"));
+  return(NORMAL);
+}
+int compound_statement()//複合文:"begin" 文 { ";" 文 } "end"
+{
+  printf("compound_statement\n");
+  if(token != TBEGIN) return(error("begin is not found"));
+  token = scan();
+  if(statement() == ERROR) return(ERROR);
+  token = scan();
+  while(token != TEND)
+  {
+    if(token != TSEMI) return(error("semicolon is not found"));
+    token = scan();
+    if(statement() == ERROR) return(ERROR);
+    token = scan();
+  }
+  
+  return(NORMAL);
+}
+int formal_parameters()//仮引数部:"(" 変数名の並び ":" 型 { ";" 変数名の並び ":" 型 } ")"
+{
+
+}
+int procedure_name()//手続き名:"名前"
+{
+
+}
+int statement()//文
+{
+
+}
